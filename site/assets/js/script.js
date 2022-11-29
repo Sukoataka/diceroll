0311265d1d2d00da60b33a129035a73c04fda1da
Sukoataka <84311134+Sukoataka@users.noreply.github.com>
2022-11-29 10:50:24 +0100

    created all pages

--- site/assets/js/script.js
+++ site/assets/js/script.js
@@ -1,1 +1,19 @@
 "use strict";
+
+document.addEventListener("DOMContentLoaded", init);
+
+function init(){
+    const playButton = document.querySelector("#play");
+    const highScoresButton = document.querySelector("#highScores");
+
+    playButton.addEventListener("click", play);
+    highScoresButton.addEventListener("click", showHighScores);
+}
+
+function play(){
+    location.href = "../pages/waitingscreen.html";
+}
+
+function showHighScores(){
+    location.href = "../pages/highscores.html";
+}
