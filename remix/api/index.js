0f5ee52163d083d0864d301843390bcb4d32af8f
Sukoataka <bob.alossery.playstation@gmail.com>
2023-05-15 11:24:40 +0200

    made get method for best users

--- remix/api/index.js
+++ remix/api/index.js
@@ -46,10 +46,25 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+app.get("/users", async (req, res) => {
+  const bestUsers = await User.findAll({
+    order: [["timesWon", "DESC"]],
+    limit: 10,
+    attributes: ["userName", "timesWon"],
+  });
+
+  let users = [];
+
+  bestUsers.forEach((user) => {
+    users.push(user.dataValues.userName);
+  });
+  res.status(200).json(users);
+ });
+
 app.post("/addUser", async (req, res) => {
   const reqUserName = req.body.userName;
   if(!reqUserName) {
-    res.status(400).json("User name is required");
+    res.status(400).json({message: "User name is required"});
     return;
   }
 
@@ -63,17 +78,17 @@ app.post("/addUser", async (req, res) => {
         userName: req.body.userName,
         timesWon: 1,
       });
-      res.status(200).send("User added successfully");
+      res.status(200).json({message: "User added successfully"});
     } else {
       const userID = doesUserExist._previousDataValues.userID;
       await User.update(
         { timesWon: doesUserExist.timesWon + 1 },
         { where: { userID: userID } }
       );
-      res.status(204).json("User updated successfully");
+      res.status(204).json({message: "User updated successfully"});
     }
   } catch (error) {
-    res.status(500).json(error);
+    res.status(500).json({error});
   }
 });
 
